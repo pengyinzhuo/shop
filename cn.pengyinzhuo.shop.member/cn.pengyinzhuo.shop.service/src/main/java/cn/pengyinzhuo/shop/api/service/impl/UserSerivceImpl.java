6c7a149b278ed6caf7c861f76c08d871d2f1fc44
Administrator <Administrator@hp-PC>
2019-04-30 16:58:44 +0800

    第三次提交

--- cn.pengyinzhuo.shop.member/cn.pengyinzhuo.shop.service/src/main/java/cn/pengyinzhuo/shop/api/service/impl/UserSerivceImpl.java
+++ cn.pengyinzhuo.shop.member/cn.pengyinzhuo.shop.service/src/main/java/cn/pengyinzhuo/shop/api/service/impl/UserSerivceImpl.java
@@ -1,6 +1,7 @@
 package cn.pengyinzhuo.shop.api.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.druid.util.StringUtils;
@@ -8,7 +9,9 @@ import com.alibaba.druid.util.StringUtils;
 import cn.pengyinzhuo.shop.api.entity.User;
 import cn.pengyinzhuo.shop.api.service.IUserService;
 import cn.pengyinzhuo.shop.common.api.ShopResult;
+import cn.pengyinzhuo.shop.common.constants.DBTableName;
 import cn.pengyinzhuo.shop.dao.IUserDAO;
+import cn.pengyinzhuo.shop.utils.DateUtils;
 import cn.pengyinzhuo.shop.utils.MD5Util;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,7 +25,7 @@ public class UserSerivceImpl implements IUserService{
 	private IUserDAO userDao;
 	
 	@Override
-	public ShopResult regist(User user) {
+	public ShopResult regist(@RequestBody User user) {
 		if(StringUtils.isEmpty(user.getUserName())) {
 			return ShopResult.build(400,"用户名不能为空");
 		}
@@ -31,15 +34,17 @@ public class UserSerivceImpl implements IUserService{
 		}
 		try {
 			
-			
-			
+			user.setCreated(DateUtils.getTimestamp());
+			user.setUpdated(DateUtils.getTimestamp());
+			String phone = user.getPhone();
+			String password = user.getPassword();
+			user.setPassword(mdhPassSalt(phone, password));
+			userDao.save(user, DBTableName.TABLE_MB_USER);
+			return ShopResult.ok();
 		}catch (Exception e){
-			
-			
+			log.error("###regist() ERROR:",e);
+			return ShopResult.build(500, "注册失败");
 		}
-		
-		
-		return null;
 	}
 
 	
